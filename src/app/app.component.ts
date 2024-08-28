a760e173e44ea0c7b27510602d63d45547603807
IT Compliance <rhodaarthur48@gmail.com>
2024-08-28 09:25:41 -0700

    Adds new invoice button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { ModalComponent } from "./components/modal/modal.component";
 import { InvoiceListComponent } from "./components/invoice-list/invoice-list.component";
+import { InvoiceComponent } from "./components/invoice/invoice.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ModalComponent, InvoiceListComponent],
+  imports: [RouterOutlet, HeaderComponent, ModalComponent, InvoiceListComponent, InvoiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { InvoiceListComponent } from "../invoice-list/invoice-list.component";
+import { InvoiceFilterComponent } from "../invoice-filter/invoice-filter.component";
+import { InvoiceNewButtonComponent } from "../invoice-new-button/invoice-new-button.component";
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [InvoiceListComponent, InvoiceFilterComponent, InvoiceNewButtonComponent],
+  templateUrl: './invoice.component.html',
+  styleUrl: './invoice.component.css'
+})
+export class InvoiceComponent {
+
+}
